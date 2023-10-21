14a8449c896f6124d0fc88ef4def420a14b0d815
JonV12 <146036546+JonV12@users.noreply.github.com>
2023-10-21 16:38:23 -0400

    Added parsing and storage for APR and Down Payment, Added APR and Down Payment fields into the main layout

--- app/src/main/java/com/practice/carloancalculator/MainActivity.java
+++ app/src/main/java/com/practice/carloancalculator/MainActivity.java
@@ -14,8 +14,13 @@ public class MainActivity extends AppCompatActivity {
     Button saveButton;
     EditText loanInput;
     EditText termInput;
+    EditText downPaymentInput;
+    EditText aprInput;
     double loanInputNUM;
     int termInputNUM;
+    double downPaymentNUM;
+    double aprNum;
+    String tempValue;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -26,12 +31,14 @@ public class MainActivity extends AppCompatActivity {
         saveButton = (Button)findViewById(R.id.saveButton);
         loanInput = (EditText)findViewById(R.id.loanInputText);
         termInput = (EditText)findViewById(R.id.termInputText);
+        downPaymentInput = (EditText)findViewById(R.id.downPaymentInputText);
+        aprInput = (EditText)findViewById(R.id.aprInputText);
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Loan Amount Parsing and Storage
-                String tempValue = loanInput.getText().toString();  //assigns user input into a String temporary field for parsing
+                tempValue = loanInput.getText().toString();  //assigns user input into a String temporary field for parsing
                 loanInputNUM = Double.parseDouble(tempValue);       //parses string temporary field and converts into a double
                 UserInputStorage.setLoanAmount(loanInputNUM);       //stores double in the UserInputStorage
 
@@ -40,9 +47,21 @@ public class MainActivity extends AppCompatActivity {
                 termInputNUM = Integer.parseInt(tempValue);         //parses string temporary field and converts into a integer
                 UserInputStorage.setLoanLength(termInputNUM);       //stores integer in the UserInputStorage
 
+                //Down Payment Parsing and Storage
+                tempValue = downPaymentInput.getText().toString();  //assigns user input into a String temporary field for parsing
+                downPaymentNUM = Double.parseDouble(tempValue);     //parses string temporary field and converts into a double
+                UserInputStorage.setDownPayment(downPaymentNUM);    //stores double in the UserInputStorage
+
+                //APR Parsing and Storage
+                tempValue = aprInput.getText().toString();          //assigns user input into a String temporary field for parsing
+                aprNum = Double.parseDouble(tempValue);             //parses string temporary field and converts into a double
+                UserInputStorage.setApr(aprNum);                    //stores double in the UserInputStorage
+
                 //Storage Validation
-                Log.d(TAG, "onClick: Loan Amount: " + UserInputStorage.getLoanAmount());    //validation to make sure its storing it correctly
-                Log.d(TAG, "onClick: Loan Term: " + UserInputStorage.getLoanLength());       //validation to make sure its storing it correctly
+                Log.d(TAG, "onClick: Loan Amount: " + UserInputStorage.getLoanAmount());
+                Log.d(TAG, "onClick: Loan Term: " + UserInputStorage.getLoanLength());
+                Log.d(TAG, "onClick: Down Payment: " + UserInputStorage.getDownPayment());
+                Log.d(TAG, "onClick: APR: " + UserInputStorage.getApr());
             }
         });
     }
